9df5ee5bc1894a1f240b22bdf461224b521bfb9d
alapinsk <a.lapinski@outlook.com>
2017-04-22 17:24:01 +0200

    initial page

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,3 +1,4 @@
+import {Page0} from "./components/page0/page0.component";
 import {Page1} from "./components/page1/page1.component";
 import {Page2} from "./components/page2/page2.component";
 import {Page3} from "./components/page3/page3.component";
@@ -5,13 +6,15 @@ import {Page4} from "./components/page4/page4.component";
 
 
 export const AppRoutes = [
-    { path: "", component: Page1 },
+    { path: "", component: Page0 },
+    { path: "page1", component: Page1 },
     { path: "page2", component: Page2 },
     { path: "page3", component: Page3 },
     { path: "page4", component: Page4 }
 ];
 
 export const AppComponents = [
+    Page0,
     Page1,
     Page2,
     Page3,
